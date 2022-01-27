345588a4797310aea4ac541738d3bb35ec337243
Deadbill <vp_pank@mail.ru>
2022-01-27 22:00:04 +0300

    react-app2-backend

--- react-app2/src/components/ProfileForm/ProfileForm.js
+++ react-app2/src/components/ProfileForm/ProfileForm.js
@@ -1,11 +1,13 @@
 import { useState } from 'react';
+import uniqId from 'uniqid';
 
-const ProfileForm = ({ user }) => {
+const ProfileForm = ({ user, onSave }) => {
 
   const [firstName, setFirstName] = useState(user.firstName);
   const [lastName, setLastName] = useState(user.lastName);
   const [photoSrc, setPhotoSrc] = useState(user.photoSrc);
   const [hobbies, setHobbies] = useState(user.hobbies);
+  const [isLoading, setIsLoading] = useState(user.isLoading);
 
   const handleFirstNameChange = (event) => {
     setFirstName(event.target.value);
@@ -20,12 +22,38 @@ const ProfileForm = ({ user }) => {
     display: 'flex',
     flexDirection: 'column',
   }
+
+  const handleOnHobbyDelete = (hobbyToDelete) => {
+    return () => {
+        const filteredHobbies = hobbies.filter((hobby) => hobby.id !== hobbyToDelete.id);
+        setHobbies(filteredHobbies);
+    }
+  }
+
+  const handleOnHobbyAdd = () => {
+    setHobbies([
+      ...hobbies,
+      {id: uniqId(), name: ''}
+    ])
+  }
+
   const handleOnHobbyChange = (editableHobby) => (event) => {
     const newHobbies = hobbies.map((hobby) => ({ ...hobby}));
-    const hobbyToUpdate = newHobbies.find((hobby) => editableHobby.id);
+    const hobbyToUpdate = newHobbies.find((hobby) => hobby.id === editableHobby.id);
     hobbyToUpdate.name = event.target.value;
     setHobbies(newHobbies)
   };
+
+  const handeProfileSave = async () => {
+  setIsLoading(true);
+
+    await onSave({
+      firstName,
+      lastName,
+      photoSrc,
+      hobbies,
+    })
+  }
 
   return (
     <div style={labelsStyles}>
@@ -46,9 +74,14 @@ const ProfileForm = ({ user }) => {
         {hobbies.map((hobby) => (
           <div key={hobby.id}>
             <input value={hobby.name} onChange={handleOnHobbyChange(hobby)}/>
+            <button onClick={handleOnHobbyDelete(hobby)}>&times;</button>
           </div>
         ))}
+        <button onClick={handleOnHobbyAdd}>Add</button>
       </div>
+      <button onClick={handeProfileSave} disabled={isLoading}>
+        {isLoading ? 'Loading' : 'Save'}
+      </button>
     </div>
   );
 };

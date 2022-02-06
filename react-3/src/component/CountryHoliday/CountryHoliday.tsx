e79ce848016cc4061d176a95ef04a20a76c24992
Deadbill <vp_pank@mail.ru>
2022-02-06 04:16:43 +0300

    App CountryHolidays

--- react-3/src/component/CountryHoliday/CountryHoliday.tsx
+++ react-3/src/component/CountryHoliday/CountryHoliday.tsx
@@ -1,7 +1,8 @@
-import { CircularProgress } from '@mui/material';
+import { Button, CircularProgress, Stack } from '@mui/material';
 import { FC, useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import getHolidays, { IHoliday } from '../api/getHolidays';
+import styles from './CountryHoliday.module.scss'
 
 const CountryHoliday: FC = () => {
   const [holidays, setHolidays] = useState<IHoliday[] | null>(null);
@@ -9,6 +10,16 @@ const CountryHoliday: FC = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const dateAsString = searchParams.get('data');
+  const navigate = useNavigate();
+
+  const handleOnBack = (countryCode: string) => {
+    navigate(`/countries/${countryCode}`)
+  };
+
+  const handleOnYear = async (countryCode: string, year: number) => {
+    const newHolidays = await getHolidays(countryCode!, year)
+    setHolidays(newHolidays)
+  };
 
   const fetchHolidays = async () => {
     const date = dateAsString
@@ -25,14 +36,35 @@ const CountryHoliday: FC = () => {
   },[]);
 
   return (
-    <div>
-      { !holidays && <CircularProgress color="secondary" />}
-
-      {holidays && holidays.map((info) => (
-        <div key={`${info.name}${info.date}`}>
-          {info.localName} ({info.name}) - {info.date}
-        </div>
-      ))}
+    <div className={styles.holiday}>
+      <div>
+        <Stack direction="row" spacing={40} sx={{ ml: 15 }}>
+          <Button variant="contained" onClick={() => handleOnBack(countryCode!)}>Go Back</Button>
+        </Stack>
+      </div>
+      <div className={styles.cont}>
+        <div className={styles.title}>Holidays</div>
+        { !holidays && <CircularProgress color="secondary" />}
+        {holidays && holidays.map((info) => (
+          <div className={styles.row} key={`${info.name}${info.date}`}>
+            {info.localName} ({info.name}) - {info.date}
+          </div>
+        ))}
+      </div>
+      <div>
+        <Stack direction="row" spacing={40} sx={{ ml: 0 }}>
+          <Button variant="contained" onClick={() => handleOnYear(countryCode!, 2023)}>2023</Button>
+        </Stack>
+        <Stack direction="row" spacing={40} sx={{ ml: 0, mt: 2 }}>
+          <Button variant="contained" color="error" onClick={() => handleOnYear(countryCode!, 2022)}>2022</Button>
+        </Stack>
+        <Stack direction="row" spacing={40} sx={{ ml: 0, mt: 2 }}>
+          <Button variant="contained" onClick={() => handleOnYear(countryCode!, 2021)}>2021</Button>
+        </Stack>
+        <Stack direction="row" spacing={40} sx={{ mr: 15, mt: 2 }}>
+          <Button variant="contained" color="error" onClick={() => handleOnYear(countryCode!, 2020)}>2020</Button>
+        </Stack>
+      </div>
     </div>
   );
 };
--- react-3/src/component/CountryInfo/CountryInfo.tsx
+++ react-3/src/component/CountryInfo/CountryInfo.tsx
@@ -1,7 +1,8 @@
-import { Avatar, CircularProgress, Stack } from '@mui/material';
+import { Avatar, Button, CircularProgress, Stack } from '@mui/material';
 import { FC, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import getCountryInfo, { ICountryInfo } from '../api/getCountryInfo';
+import styles from './CountryInfo.module.scss';
 
 const CountryInfo: FC = () => {
   const [countryInfo, setCountryInfo] = useState<ICountryInfo | null>(null);
@@ -19,6 +20,14 @@ const CountryInfo: FC = () => {
     navigate(`/countries/${countryCode}`)
   };
 
+  const handleOnClick  = () => {
+    navigate(`/`)
+  };
+
+  const handleOnHoliday = (countryCode: string) => {
+    navigate(`/countries/${countryCode}/holidays`)
+  };
+
   useEffect(() => {
     fetchCountryInfo();
   },[countryCode]);
@@ -27,14 +36,28 @@ const CountryInfo: FC = () => {
     <div>
       {!countryInfo && <CircularProgress color="secondary" />}
       {countryInfo && (
-        <div>
-          <div>{countryInfo.commonName} ({countryInfo.officialName}) - {countryInfo.region}</div>
-          <div>{countryInfo.countryCode}</div>
-          <div>
-            {countryInfo.borders.map(({ countryCode }) => (
-              <Avatar onClick={handleCountryClick(countryCode)}>{countryCode}</Avatar>
+        <div className={styles.cont}>
+          <div className={styles.name}>{countryInfo.commonName} ({countryInfo.officialName}) - {countryInfo.region}</div>
+          <div className={styles.code}>{countryInfo.countryCode}</div>
+          <Stack direction="row" spacing={2} sx={{alignItems:'center', display:'inline-flex', flexDirection:'row'}}>
+            {countryInfo.borders.map((info) => (
+              <Avatar onClick={handleCountryClick(info.countryCode)} sx={{ width: 70, height: 70}}>
+                <img
+                  loading="lazy"
+                  width="20"
+                  src={`https://flagcdn.com/w20/${info.countryCode.toLowerCase()}.png`}
+                  srcSet={`https://flagcdn.com/w40/${info.countryCode.toLowerCase()}.png 2x`}
+                  alt={`Flag of ${info.officialName}`}
+                  className={styles.image}
+                />
+                {info.countryCode}
+              </Avatar>
             ))}
-          </div>
+          </Stack>
+          <Stack direction="row" spacing={40} sx={{mt:20}}>
+            <Button variant="contained" color="success" onClick={handleOnClick}>Go Back</Button>
+            <Button variant="contained" color="error" onClick= {() => handleOnHoliday(countryInfo?.countryCode)}>Show Holiday</Button>
+          </Stack>
         </div>
       )}
     </div>
